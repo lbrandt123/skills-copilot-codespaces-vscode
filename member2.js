a0ef058b157fcac6954de5419287d233c18decdc
lbrandt123 <lbrandt123@gmail.com>
2023-08-01 00:59:09 +0000

    Copilot second commit

---
+++ member2.js
@@ -0,0 +1,13 @@
+function skillsMember(){
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/skills-member.html',
+        controller: 'SkillsMemberCtrl',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '=',
+            skills: '='
+        }
+    };
+}
